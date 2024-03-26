f4a86bd7af390dafaa87ad22d1047132eb503fe0
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-03-27 02:46:49 +0800

    fix: 刪除深色模式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { BackgroundGradient } from "@/components/ui/background-gradient";
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center bg-background bg-dot-black/[0.2]">
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       {/* content */}
       <div className="flex flex-col items-center justify-center gap-8 pb-20 pt-10">
         <div className="flex flex-col items-center justify-center gap-8">
