7fb715cf1b9a93673fc476b1aa3f88b9533116fe
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-03-27 10:30:24 +0800

    fix: 修改selfCard 樣式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,16 @@ export default function Home() {
             <div className="h-[15rem] w-[15rem] rounded-tl-lg rounded-tr-lg bg-primary-light">
               <Image src="/images/me.png" alt="me" width={600} height={600} />
             </div>
-            <div className="flex flex-col gap-2 p-4">
-              <div className="font-sans text-xl font-black text-background">
-                SHUSHU{" "}
-                <span className="text-base font-medium text-background">
-                  {" "}
-                  # 0829{" "}
-                </span>
+            <div className="flex flex-col gap-1 p-4">
+              <div className="flex items-end gap-2">
+                <div className="font-notoSans text-xl font-black text-background">
+                  SHUSHU
+                </div>
+                <div className="font-mono text-base font-medium text-background">
+                  #0829
+                </div>
               </div>
-              <div className="font-sans text-slate-300">
+              <div className="font-sans text-slate-200">
                 shushu90829@gmail.com
               </div>
             </div>
